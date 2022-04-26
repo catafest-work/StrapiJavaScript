c0213b2deb87f51abb31d82da645dadb154c281f
catalin.festila <catafest76@gmail.com>
2022-04-26 09:27:50 +0300

    push changes for orders.js

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -11,10 +11,31 @@ module.exports = createCoreController('api::order.order', ({strapi}) => ({
   confirmOrder: async(ctx, next) => {
      //ctx.body = "ok";
      const {id} = ctx.request.params;
-     console.log(id);
+     const order = await strapi.entityService.findOne("api::order.order", id);
+     await strapi.entityService.update("api::order",
+     {
+       id, 
+       data: {
+         confirmed: true,
+         date_confirmed: new Data(),
+         
+       },
+     });
+     // send an email after confirmation
+     return 
+     {
+       message: "confirmed";
+     };
+     //id; // this will be used on postman to see how this send data request
   },
   /*find: async (ctx, next) => {
     const (data, met) = await super.find(ctx);
     return
   }*/
+  create: async (ctx, next) => {
+    const user = ctx.state.user;
+    const {data, meta} = this.create(ctx);
+    data.user = user;
+    return {data, meta}
+  }
 }));
